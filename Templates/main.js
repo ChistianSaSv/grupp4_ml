1176cd38943d3d68db6c078de7aecd8afd155325
malmoff <81243022+malmoff@users.noreply.github.com>
2021-03-31 22:22:51 +0200

    Update main.js

--- Templates/main.js
+++ Templates/main.js
@@ -1,1 +1,5 @@
 
+$('#test-btn').click(async function() {
+  let genreValue = $('#genre-input').val()
+  let publisherValue = $('#publisher-input').val()
+  let platformValue = $('#platform-input').val()
